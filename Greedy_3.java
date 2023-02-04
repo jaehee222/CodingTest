690da63e30c7792d5915118805379df2d8842c76
jaeheelee <124273674+jaehee222@users.noreply.github.com>
2023-02-04 17:22:17 +0900

    Create Greedy_3.java

---
+++ Greedy_3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Greedy_3 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int result = 0;
+
+        while (N != 1) {
+            result++;
+            if (N % K == 0) {
+                N = N / K;
+            } else {
+                N -= 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
